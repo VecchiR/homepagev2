d2c325dc65893593eb06b57fdb4aaa7c60a9f456
VecchiR <79600745+VecchiR@users.noreply.github.com>
2024-10-19 15:00:36 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'Rafael Vecchi',
+        }),
+    ],
+    module: {
+        rules: [
+         {
+           test: /\.(png|svg|jpg|jpeg|gif)$/i,
+           type: 'asset/resource',
+         },
+         {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
+};
